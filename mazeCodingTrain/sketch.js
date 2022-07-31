354f13457cfa0f1ada3a71e8f67936310a4ad323
Greg K <gkreisman_cep@qq.com>
2022-08-01 00:47:04 +0800

    made it a 2 d array again it's easyer

--- mazeCodingTrain/sketch.js
+++ mazeCodingTrain/sketch.js
@@ -1,6 +1,6 @@
 let cols, rows;
 let w = 60; // width of the cell used in cell class
-let grid= [];
+let grid =[];
 
 
 
@@ -9,18 +9,22 @@ function setup(){
     cols = floor(width/w);
     rows= floor(height/w);
     // put 2 d cell positions in a 1 d array
-     
-
-    for (let j =0; j < rows; j++){
-        for (let i =0; i < cols; i++){
-            grid.push(new Cell(i,j));
-        }
-    }
-
+    gridSetup(); // runs on global grid don't need to pass or return
+    grid[0][0].walls = [false,false,false,false]
+    grid[1][0].walls = [false,false,false,false]
     background(0)
     showGrid();
 }
 
+function gridSetup(){
+    for (let j =0; j < rows; j++){
+        let row = [];
+        for (let i =0; i < cols; i++){
+            row.push(new Cell(i,j));
+        }
+        grid.push(row)
+    }
+}
  
 function index(i, j) {
     if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
@@ -33,8 +37,10 @@ function index(i, j) {
 
 
 function showGrid(){
-     for (let i =0; i<grid.length;i++){
-        grid[i].showCell();
+     for (let j =0; j< rows;j++){
+        for(let i =0; i< cols;i++){
+        grid[j][i].showCell();
+        }
 
      }
     }
