f356bb171c6436599008a9b2c3fb3ff411e1ac39
Greg K <gkreisman_cep@qq.com>
2022-08-01 21:44:24 +0800

    slider and button

--- binaryTree/sketch.js
+++ binaryTree/sketch.js
@@ -1,6 +1,7 @@
 let cols, rows;
 let w = 60; // width of the cell used in cell class
 let grid =[];
+let mybutton, myslider;
 
 
 
@@ -10,9 +11,22 @@ function setup(){
     rows= floor(height/w);
     // put 2 d cell positions in a 1 d array
     gridSetup(); // runs on global grid don't need to pass or return
-    grid[0][0].doBinary();
+    mybutton = createButton("click to change");
+    mybutton.mousePressed(doItAll)
+    myslider = createSlider(10, 120, 60, 1);
     runGrid();
     background(0)
+    showGrid();
+}
+
+function doItAll(){
+    background(0);
+    w = myslider.value();
+    cols = floor(width/w);
+    rows= floor(height/w);
+    grid =[];
+    gridSetup();
+    runGrid();
     showGrid();
 }
 
