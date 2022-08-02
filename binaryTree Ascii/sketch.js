6faa5bf747471729247fe2b00c2439d1af0d19cd
Greg K <gkreisman_cep@qq.com>
2022-08-02 17:04:02 +0800

    ascii function to pre

--- binaryTree Ascii/sketch.js
+++ binaryTree Ascii/sketch.js
@@ -1,11 +1,14 @@
 let cols, rows;
-let w = 60; // width of the cell used in cell class
+let w = 120; // width of the cell used in cell class
 let grid =[];
 let mybutton, myslider;
+let asciiArt="";
+let textArt; // for pre
 
 
 
 function setup(){
+    textArt = select('#textart');
     createCanvas(600,600);
     cols = floor(width/w);
     rows= floor(height/w);
@@ -17,6 +20,7 @@ function setup(){
     runGrid();
     background(0)
     showGrid();
+    showGridAscii();
 }
 
 function doItAll(){
@@ -28,6 +32,7 @@ function doItAll(){
     gridSetup();
     runGrid();
     showGrid();
+    showGridAscii();
 }
 
 function gridSetup(){
@@ -65,7 +70,42 @@ function showGrid(){
     
     
   
-  
+function showGridAscii(){
+    //only need to check east and south
+
+    // makes a top edge of as many cols as there are
+    asciiArt ="+"
+    asciiArt += "---+".repeat(cols);// repeat works likes the * "str" in python
+    asciiArt += "\n" // puts a line break on it so it forms a grid
+    //
+
+
+    for (let j =0; j< rows;j++){
+        let middle = "|"; // adds in the west wall
+        let bottom = "+" 
+        for(let i =0; i< cols;i++){
+            if (grid[j][i].walls.east){
+                middle += "   |"; // three spaces and a pipe if an east wall
+            }else{
+                middle+="    "; // otherwise for spaces
+            }
+            if (grid[j][i].walls.south){
+                bottom += "---+";
+            }else{
+                bottom += "   +";
+            }
+
+    
+        }
+        middle +="\n" // east most line breaks
+        bottom +="\n" 
+        // add to asciiArt
+        asciiArt += middle
+        asciiArt += bottom;
+
+    }
+    textArt.html(asciiArt)
+    } 
   
   
   
