24218174be771a4a18ed31dca245447517a70a19
Greg K <gkreisman_cep@qq.com>
2022-08-06 21:44:43 +0800

    problems with algorithm

---
+++ binaryTreeDijkstra/cell.js
@@ -0,0 +1,108 @@
+class Cell {
+    constructor(i,j){
+        //i and j are values in grid
+        //x and y are values on canvas mult by cell width w is global
+        this.i =i;
+        this.j=j;
+        this.walls ={north:true,south:true,west:true,east:true}
+        this.visited = false;
+        this.x = i*w;
+        this.y = j*w;
+        this.connected = [];
+         
+        this.distance=0;
+    }
+
+
+    doBinary(){
+        // pic north or eastern wall and erase
+        // if top row erase eastern wall
+        // if eastern col erase top
+        // else pick east or north
+        
+        
+        if (this.j == 0){
+            // if the cell is the top row and not the far east
+            // erase the east wall of this and west wall of eastern neighbor
+            if (this.i<cols-1){
+                this.walls.east = false;
+                grid[this.j][this.i+1].walls.west = false;
+                //add connection to the east
+                this.connected.push("east")
+                grid[this.j][this.i+1].connected.push("west")
+            }
+        }
+
+        else if (this.i == cols-1){
+            this.walls.north = false;
+            grid[this.j-1][this.i].walls.south = false;
+            this.connected.push("north")
+            grid[this.j-1][this.i].connected.push("south")
+        }
+        
+        else{
+            let flip = random(["heads", "tails"])
+            print(flip)
+
+            if (flip ==="heads"){
+                //erase north and south of above
+                this.walls.north = false;
+                grid[this.j-1][this.i].walls.south = false;
+                this.connected.push("north")
+                grid[this.j-1][this.i].connected.push("south")
+            }else{
+                this.walls.east = false;
+                grid[this.j][this.i+1].walls.west = false;
+                this.connected.push("east")
+                grid[this.j][this.i+1].connected.push("west")
+            }
+        }
+
+         
+        // have a working neighbors list
+  
+    }
+
+    showCell(){
+        
+        stroke(255, 128, 0);
+        strokeWeight(map(w,10,120,1,20))
+        if (this.walls.north) {
+        line(this.x, this.y, this.x + w, this.y);
+        }
+        if (this.walls.east) {
+        line(this.x + w, this.y, this.x + w, this.y + w);
+        }
+        if (this.walls.south) {
+        line(this.x + w, this.y + w, this.x, this.y + w);
+        }
+        if (this.walls.west) {
+        line(this.x, this.y + w, this.x, this.y);
+        }
+
+        if (this.visited) {
+        noStroke();
+        fill(255, 255, 0);
+        rect(this.x, this.y, w-2, w-2);
+        }
+        //stroke(255);
+        noStroke();
+        fill(0,255,0)
+        textSize(w/2)
+        text(this.distance,this.x+w/2, this.y+w/2)
+    
+
+    }
+/*
+    dijkstra(){
+        
+        if (this.rootI === this.i && this.rootJ === this.j){
+            this.distance = 0;
+        }else{
+            let distCount =1;
+            while 
+        }
+
+    }
+    */
+}
---
+++ binaryTreeDijkstra/sketch.js
@@ -0,0 +1,209 @@
+let cols, rows;
+let w = 50; // width of the cell used in cell class
+let grid =[];
+let mybutton, myslider;
+let asciiArt="";
+let textArt; // for pre
+let runners= [];
+let numOfRunners =20;
+// runner is its own class with own method to show so dont use visited
+
+
+
+function setup(){
+    textArt = select('#textart');
+    createCanvas(1000,1000);
+    frameRate(10)
+    textAlign(CENTER,CENTER)
+    cols = floor(width/w);
+    rows= floor(height/w);
+    // put 2 d cell positions in a 1 d array
+    gridSetup(); // runs on global grid don't need to pass or return
+    mybutton = createButton("click to change");
+    mybutton.mousePressed(doItAll)
+    myslider = createSlider(10, 100, 50, 10);
+    runGrid();
+    background(0)
+    showGrid();
+
+    // set up runners
+    for (let i =0; i<numOfRunners; i++){
+        runners[i] = new Runner (floor(cols/2),floor(rows/2),color(random(255),random(255),random(255))); // need to do runner before show ascii
+    }
+   // showGridAscii();   // must show ascii after runners
+    dijkstra();
+}
+
+function draw(){
+    background(0);
+    // show grids
+    showGrid()
+    //showGridAscii();
+
+    //loop through runners
+    for (let i = 0; i < runners.length;i++){
+    runners[i].moveRunner();
+    runners[i].showRunner()
+    }
+     
+    
+}
+
+
+
+function doItAll(){
+    background(0);
+    w = myslider.value();
+    cols = floor(width/w);
+    rows= floor(height/w);
+    grid =[];
+    gridSetup();
+    runGrid();
+    showGrid();
+    // reset runners
+    for (let i =0; i<numOfRunners; i++){
+        runners[i] = new Runner (floor(cols/2),floor(rows/2),color(random(255),random(255),random(255))); // need to do runner before show ascii
+    }
+     
+    //showGridAscii(); // need to do runner before show ascii
+     
+}
+
+function gridSetup(){
+    // sets up a blank 2d grid using the global grid array
+    for (let j =0; j < rows; j++){
+        let row = [];
+        for (let i =0; i < cols; i++){
+            row.push(new Cell(i,j));
+        }
+        grid.push(row)
+    }
+}
+ 
+ 
+function runGrid(){
+    for (let j =0; j< rows;j++){
+       for(let i =0; i< cols;i++){
+       grid[j][i].doBinary();
+       }
+
+    }
+   }
+
+
+
+function showGrid(){
+     for (let j =0; j< rows;j++){
+        for(let i =0; i< cols;i++){
+        grid[j][i].showCell();
+        }
+
+     }
+    }
+    
+    
+    
+  
+function showGridAscii(){
+    //only need to check east and south
+
+    // makes a top edge of as many cols as there are
+    asciiArt ="+"
+    asciiArt += "---+".repeat(cols);// repeat works likes the * "str" in python
+    asciiArt += "\n" // puts a line break on it so it forms a grid
+    //
+
+
+    for (let l =0; l< rows;l++){
+        let middle = "|"; // adds in the west wall
+        let bottom = "+" 
+        for(let k =0; k< cols;k++){
+
+            if (runners[0].j == l && runners[0].i == k){  // just one
+                if (grid[l][k].walls.east){
+                    middle += " * |"; // three spaces and a pipe if an east wall
+                }else{
+                    middle+=" *  "; // otherwise for spaces
+                }
+            }else{
+                if (grid[l][k].walls.east){
+                    middle += "   |"; // three spaces and a pipe if an east wall
+                }else{
+                    middle+="    "; // otherwise for spaces
+                }
+            }
+            if (grid[l][k].walls.south){
+                bottom += "---+";
+            }else{
+                bottom += "   +";
+            }
+
+    
+        }
+        middle +="\n" // east most line breaks
+        bottom +="\n" 
+        // add to asciiArt
+        asciiArt += middle
+        asciiArt += bottom;
+
+    }
+    textArt.html(asciiArt)
+    } 
+
+  
+ function dijkstra(){
+    
+    let root = grid[0][0];
+    root.distance = 0;
+    let distCount =1;
+    let current = root;
+    let front = getfront(current, distCount);
+    console.log(front)
+   // while (front.length>0){
+        distCount++
+        newfront = []
+        for (let x =0; x < front.length; x++){
+            print("thisisit",front[x])
+            addtonewfront= getfront(front[x],distCount);
+            //newfront.push(addtonewfront);
+        }
+       // front= newfront;
+
+    }
+     
+
+ //} 
+
+
+ function getfront(mycell, distCount){
+    let result =[];
+    let neighbor = mycell.connected
+
+    for(let f = 0; f<neighbor.length;f++){
+        if (neighbor[f] == "north"){
+            grid[mycell.j-1][mycell.i].distance=distCount;
+            result.push(grid[mycell.j-1][mycell.i])
+
+        }
+        else if (neighbor[f] == "south"){
+           grid[mycell.j+1][mycell.i].distance=distCount;
+            result.push(grid[mycell.j+1][mycell.i])
+
+        }
+        else if (neighbor[f] == "east"){
+           grid[mycell.j][mycell.i+1].distance=distCount;
+            result.push(grid[mycell.j][mycell.i+1])
+
+        }
+        else if (neighbor[f] == "west"){
+            grid[mycell.j][mycell.i-1].distance=distCount;
+            result.push(grid[mycell.j][mycell.i-1])
+
+        }
+    }
+    return result;
+
+
+ }
+
+   
